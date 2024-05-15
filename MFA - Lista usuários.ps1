5e3940a6cd697e118e0f90563d13406185d68441
brenoacp <36081712+brenoacp@users.noreply.github.com>
2024-05-15 16:17:42 -0300

    Add files via upload

---
+++ MFA - Lista usuários.ps1
@@ -0,0 +1,1 @@
+Get-MsolUser -all | select DisplayName,UserPrincipalName,@{N="MFA Status"; E={if( $_.StrongAuthenticationRequirements.State -ne $null) {$_.StrongAuthenticationRequirements.State} else { "Disabled"}}}
